e427f35783797a77329533c0410eaf6625bc57b6
Elizabeth Rego <elizabethmrego@gmail.com>
2016-02-21 12:34:03 -0500

    Added polyfill for CustomEvent

--- www/unimag-swiper.js
+++ www/unimag-swiper.js
@@ -59,3 +59,24 @@ Swiper.on = function (event, callback, scope) {
 };
 
 module.exports = Swiper;
+
+/*
+ * Polyfill for adding CustomEvent
+ * see : https://developer.mozilla.org/fr/docs/Web/API/CustomEvent,
+         http://stackoverflow.com/questions/25579986/
+ */
+
+if (!window.CustomEvent) { // Create only if it doesn't exist
+    (function () {
+        function CustomEvent ( event, params ) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            var evt = document.createEvent( 'CustomEvent' );
+            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+            return evt;
+        };
+
+        CustomEvent.prototype = window.Event.prototype;
+
+        window.CustomEvent = CustomEvent;
+    })();
+}
