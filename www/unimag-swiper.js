67c5fd001120ba557b72c4f2019763c1e2125c4e
Elizabeth Rego <elizabethmrego@gmail.com>
2016-02-22 09:51:29 -0500

    Update unimag-swiper.js

--- www/unimag-swiper.js
+++ www/unimag-swiper.js
@@ -61,22 +61,23 @@ Swiper.on = function (event, callback, scope) {
 module.exports = Swiper;
 
 /*
- * Polyfill for adding CustomEvent
+ * Polyfill for adding CustomEvent -- Copy uncommented lines below into your
+ * application if you get  Reference Error: CustomEvent is undefined
  * see : https://developer.mozilla.org/fr/docs/Web/API/CustomEvent,
          http://stackoverflow.com/questions/25579986/
- */
 
-if (!window.CustomEvent) { // Create only if it doesn't exist
-    (function () {
-        function CustomEvent ( event, params ) {
-            params = params || { bubbles: false, cancelable: false, detail: undefined };
-            var evt = document.createEvent( 'CustomEvent' );
-            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-            return evt;
-        };
-
-        CustomEvent.prototype = window.Event.prototype;
-
-        window.CustomEvent = CustomEvent;
-    })();
-}
+	if (!window.CustomEvent) { // Create only if it doesn't exist
+	    (function () {
+	        function CustomEvent ( event, params ) {
+	            params = params || { bubbles: false, cancelable: false, detail: undefined };
+	            var evt = document.createEvent( 'CustomEvent' );
+	            evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+	            return evt;
+	        };
+	
+	        CustomEvent.prototype = window.Event.prototype;
+	
+	        window.CustomEvent = CustomEvent;
+	    })();
+	}
+*/
